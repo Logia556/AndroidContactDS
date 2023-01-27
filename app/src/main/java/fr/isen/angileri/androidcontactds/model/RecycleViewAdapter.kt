cea5b5be848bda0b01409858482d2c34a709d867
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-01-27 17:20:02 +0100

    apk avec le recyclerview

--- app/src/main/java/fr/isen/angileri/androidcontactds/model/RecycleViewAdapter.kt
+++ app/src/main/java/fr/isen/angileri/androidcontactds/model/RecycleViewAdapter.kt
@@ -54,3 +54,39 @@ class RecycleViewAdapter (private val mContacts: List<Contact>) : RecyclerView.A
         }
 
     }
+/*abstract class ContactsViewAdapter(private val contacts: List<Results>) : RecyclerView.Adapter<ContactsViewAdapter.ContactsViewHolder>() {
+    // provide a default constructor
+
+
+
+    inner  class ContactsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val firstName: TextView = itemView.findViewById(R.id.contactNom)
+        val lastName: TextView = itemView.findViewById(R.id.contactPrenom)
+        val address: TextView = itemView.findViewById(R.id.contactAddress)
+        val email: TextView = itemView.findViewById(R.id.contactMail)
+        val picture: ImageView = itemView.findViewById(R.id.contactImage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val contactLayout = inflater.inflate(R.layout.contact_item, parent, false)
+        return ContactsViewHolder(contactLayout)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
+        val contact = contacts[position]
+        holder.firstName.text = contact.getFirstName()
+        holder.lastName.text = contact.getLastName()
+        holder.address.text = contact.getAddress()
+        holder.email.text = contact.email
+
+        if (contact.picture != null) {
+            Picasso.get().load(contact.getPicture())
+        }
+    }
+}*/
